96405cc5b0dbff5034d5dfe314e3f2435ac25234
Tomasz Heflik <heflik@gmail.com>
2018-03-17 21:16:45 +0100

    Add new data definition

---
+++ L.com/Exercise Files/Chapter 2/02_02/End/src/main/java/com/lynda/common/data/entity/TicketEntity.java
@@ -0,0 +1,23 @@
+package com.lynda.common.data.entity;
+
+public class SalesOrderLineEntity {
+
+    private int ticketId;
+    private String description;
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(int ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
